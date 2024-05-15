bbb9503159144ce07a02cfe2517b03392a2c733f
Fatemeh-Abbasi512 <166016372+Fatemeh-Abbasi512@users.noreply.github.com>
2024-05-15 05:36:03 -0700

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,23 @@ function solution(str){
     const result = reverse.join('');
     return result;
     };
+
+
+//Simple Change Machine
+
+    function changeMe(moneyIn){
+  if (moneyIn === '£5') {
+   return "20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p";
+  }else if (moneyIn === '£2') {
+    return "20p 20p 20p 20p 20p 20p 20p 20p 20p 20p" ;
+  }else if (moneyIn === '£1') {
+    return "20p 20p 20p 20p 20p";
+  }else if (moneyIn === '50p') {
+    return "20p 20p 10p";
+  }else if (moneyIn === '20p') {
+    return "10p 10p";
+  }else{
+    return moneyIn;
+  }
+    };
+   
