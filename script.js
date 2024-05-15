12a2f450a86aea3e33e12cabf5515b587b37065a
Fatemeh-Abbasi512 <166016372+Fatemeh-Abbasi512@users.noreply.github.com>
2024-05-15 05:11:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,12 @@
 function smash(words) {
     return words.join(' '); 
 };
+
+
+//Reversed Strings
+function solution(str){
+    const strChunks = str.split("");
+    const reverse = strChunks.reverse();
+    const result = reverse.join('');
+    return result;
+    };
